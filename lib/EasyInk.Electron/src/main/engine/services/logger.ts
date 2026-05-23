412154020f32f47f2900680e7ab132292e20a836
Yuan <qa894178522@qq.com>
2026-05-23 20:46:52 +0800

    refactor: remove electron package

--- lib/EasyInk.Electron/src/main/engine/services/logger.ts
+++
@@ -1,9 +0,0 @@
-export type LogLevel = 'info' | 'error'
-
-export interface Logger {
-  log: (level: LogLevel, message: string, jobId?: string) => void
-}
-
-export const nullLogger: Logger = {
-  log: () => {}
-}
--- lib/EasyInk.Electron/src/renderer/src/main.ts
+++
@@ -1,8 +0,0 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
-import { router } from './router'
-
-createApp(App).use(createPinia()).use(router).mount('#app')
