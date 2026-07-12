d3e7e16a46cc452e63a99062fd10c0c1c4a45b99
Leo Yang <qa894178522@qq.com>
2026-07-12 16:57:45 +0800

    refactor(designer): derive rotatable capability from manifests

--- packages/designer/src/materials/capabilities.ts
+++ packages/designer/src/materials/capabilities.ts
@@ -1,18 +1,18 @@
+import type { MaterialManifest } from '@easyink/core'
 import type { MaterialNode } from '@easyink/schema'
-import type { MaterialDefinition } from '../types'
 
 interface MaterialLookup {
-  getMaterial: (type: string) => Pick<MaterialDefinition, 'capabilities'> | undefined
+  getMaterialManifest: (type: string) => MaterialManifest | undefined
 }
 
-export function isMaterialRotatable(material: Pick<MaterialDefinition, 'capabilities'> | undefined): boolean {
-  return material?.capabilities.rotatable !== false
+export function isMaterialRotatable(material: MaterialManifest | undefined): boolean {
+  return material?.common.interaction.rotatable !== false
 }
 
 export function isElementRotatable(store: MaterialLookup, node: Pick<MaterialNode, 'type'> | null | undefined): boolean {
   if (!node)
     return false
-  return isMaterialRotatable(store.getMaterial(node.type))
+  return isMaterialRotatable(store.getMaterialManifest(node.type))
 }
 
 export function filterRotatableElements<T extends Pick<MaterialNode, 'type'>>(store: MaterialLookup, nodes: readonly T[]): T[] {
